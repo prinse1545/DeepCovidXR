d1c9f5654b39d08b362d06afcf9a79b6aec84ee8
prinse1545 <philipp.msrivastava@gmail.com>
2021-06-22 20:24:09 +0000

    figured out how to remove last layer and add new layer

--- kaggle_covid_models.py
+++ kaggle_covid_models.py
@@ -6,7 +6,8 @@
 
 # Importing models
 from covid_models import DenseNet
-from keras.layers import Dense
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense
 
 # initializing model with access to weights
 dense_init = DenseNet("/data/covid_weights/DenseNet_224_up_crop.h5");
@@ -17,7 +18,9 @@ dense_built = dense_init.buildBaseModel(224);
 # freezing all layers but the last
 dense_built = dense_init.freeze(dense_built);
 
-# editing last layer to be four class model
-dense_built.layers.pop();
-dense_built.add(Dense(4, activation="sigmoid"));
-print(dense_built.summary());
+# editing last layer to be four class model and creating new model
+dense_kbuilt = Model(inputs = dense_built.input, outputs = Dense(4,
+    activation = "sigmoid", name="last")(dense_built.layers[-2].output));
+
+print(dense_kbuilt.summary())
+print(len(dense_kbuilt.layers))
