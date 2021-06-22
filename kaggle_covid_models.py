feabf567dee40391fe7b4740c3a7738e0a151995
prinse1545 <philipp.msrivastava@gmail.com>
2021-06-22 22:09:58 +0000

    reformatted a bit

--- kaggle_covid_models.py
+++ kaggle_covid_models.py
@@ -5,22 +5,31 @@
 # 2021-06-22
 
 # Importing models
+import pydicom
 from covid_models import DenseNet
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Dense
 
-# initializing model with access to weights
-dense_init = DenseNet("/data/covid_weights/DenseNet_224_up_crop.h5");
-        
-# building base model
-dense_built = dense_init.buildBaseModel(224);
+def buildModel():
 
-# freezing all layers but the last
-dense_built = dense_init.freeze(dense_built);
+    # initializing model with access to weights
+    dense_init = DenseNet("/data/covid_weights/DenseNet_224_up_crop.h5");
+            
+    # building base model
+    dense_built = dense_init.buildBaseModel(224);
+    
+    # freezing all layers but the last
+    dense_built = dense_init.freeze(dense_built);
+    
+    # editing last layer to be four class model and creating new model
+    dense_kbuilt = Model(inputs = dense_built.input, outputs = Dense(4,
+        activation = "sigmoid", name="last")(dense_built.layers[-2].output));
+    # returning model
+    return dense_kbuilt;
 
-# editing last layer to be four class model and creating new model
-dense_kbuilt = Model(inputs = dense_built.input, outputs = Dense(4,
-    activation = "sigmoid", name="last")(dense_built.layers[-2].output));
+def generateImages():
 
-print(dense_kbuilt.summary())
-print(len(dense_kbuilt.layers))
+    # generating cropped images using image utils
+    print(pydicom.filereader.dcmread("/data/kaggle_data/train/005057b3f880/e34afce999c5/3019399c31f4.dcm"))
+
+generateImages();
