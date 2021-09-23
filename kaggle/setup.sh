c1760435dcad607b7cf6bd03890fec3990c0066e
prinse1545 <philipp.msrivastava@gmail.com>
2021-09-23 18:20:29 +0000

    big restructure / polish started

---
+++ kaggle/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Filename: setup.sh
+# Description: This file sets up all required
+# dependencies and the environment needed for
+# the project to run.
+# 
+# 2021.09.22
+
+# configuring paths and installing packages that make cuda work
+apt-get update
+apt-get install -y libsm6 libxext6 libxrender-dev
+# installing python dependencies
+pip3 install -r requirements.txt
+# getting absolute path of parent directory
+COVID_PATH=$( cd .. && pwd )
+
+# setting python paths
+echo "export PYTHONPATH="${PYTHONPATH}:${COVID_PATH}"" >> ~/.bashrc
+# reloading bashrc
+exec bash
